9389a5e86e0e623c4a1e2f5074e48cc156f9ffc8
Ron Dilley <ron.dilley@gmail.com>
2018-01-03 16:50:55 -0800

    Adding cache source files

---
+++ src/cache.h
@@ -0,0 +1,75 @@
+/****
+ *
+ * Headers for Process cache
+ * 
+ * Copyright (c) 2006-2018, Ron Dilley
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****/
+
+#ifndef CACHE_DOT_H
+#define CACHE_DOT_H
+
+/****
+ *
+ * defines
+ *
+ ****/
+
+/****
+ *
+ * includes
+ *
+ ****/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <sysdep.h>
+
+#ifndef SYSDEP_DOT_H
+#error something is messed up
+#endif
+
+#include <common.h>
+#include "wirespy.h"
+#include "hash.h"
+#include "util.h"
+#include "mem.h"
+
+/****
+ *
+ * consts & enums
+ *
+ ****/
+
+/****
+ *
+ * typedefs & structs
+ *
+ ****/
+
+/****
+ *
+ * function prototypes
+ *
+ ****/
+
+int writeFlowState( char *out_fName );
+int readFlowState( char *in_fName );
+
+#endif /* CACHE_DOT_H */
